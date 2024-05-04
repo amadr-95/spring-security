f2ee6edfdecb1ff5762b5e29274af41d289a8469
Amador <amasabcar@outlook.com>
2024-05-04 17:18:45 +0200

    configure spring security to basic Auth

---
+++ src/main/java/com/example/spring/security/ApplicationSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.spring.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(authz -> authz
+                        .anyRequest()
+                        .authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+}
